502e2db894baaaa58c5e6c6e37b4d87c1fdd9df1
KSTU Student <KSTU.Student@unet.kstu.kz>
2026-03-26 11:13:58 +0500

    Добавлены базовые функции калькулятора

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Простой калькулятор")
+    print("Сложить 5 + 3 =", add(5, 3))
+    print("Вычитание 10 - 4 =", substract(10, 4))
